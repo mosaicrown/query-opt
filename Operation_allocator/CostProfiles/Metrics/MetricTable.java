df455845e8f19fb53dfe2c098a1dec9149563751
Dario Facchinetti <dario.fad@gmail.com>
2018-02-05 11:01:36 +0100

    Small fix to simplify cost engine structure.

--- Operation_allocator/CostProfiles/Metrics/MetricTable.java
+++ Operation_allocator/CostProfiles/Metrics/MetricTable.java
@@ -4,41 +4,41 @@ public class MetricTable {
     /**
      * Input data metrics
      */
-    double inputSize;                //total size of input data in KBs
-    double inputTupleSize;           //size of input tuple in KBs
+    public double inputSize;                //total size of input data in KBs
+    public double inputTupleSize;           //size of input tuple in KBs
     /**
      * Output data metrics
      */
-    double outputSize;                //total size of output data in KBs
-    double outputTupleSize;           //size of output tuple in KBs
+    public double outputSize;                //total size of output data in KBs
+    public double outputTupleSize;           //size of output tuple in KBs
     /**
      * Machine architecture metrics
      */
-    double CPI = 2.3;                //average standard complexity adimensional
-    double CT = 0.5 * 10e-9;         //average clock time in seconds
+    public double CPI = 2.3;                //average standard complexity adimensional
+    public double CT = 0.5 * 10e-9;         //average clock time in seconds
     /**
      * Operation metrics
      */
-    double CPU_time;                 //estimated cpu execution time
-    double IO_time;                  //estimated io execution time
+    public double CPU_time;                 //estimated cpu execution time
+    public double IO_time;                  //estimated io execution time
     /**
      * Algorithm metrics
      */
-    double Not;                      //number of tuples
-    double Nop_cpu;                  //estimated number of CPU operations
-    double Nop_io;                   //estimated number of I/O operations
+    public double Not;                      //number of tuples
+    public double Nop_cpu;                  //estimated number of CPU operations
+    public double Nop_io;                   //estimated number of I/O operations
     /**
      * Economic cost metrics
      */
     //Motion
-    double Mc;                       //estimated cost of motion as dollars per program
-    double Km;                       //provider inbound/outbound specific cost of motion as dollars per KB
+    public double Mc;                       //estimated cost of motion as dollars per program
+    public double Km;                       //provider inbound/outbound specific cost of motion as dollars per KB
     //Execution
-    double Ec;                       //estimated cost of execution as dollars per program
-    double Kcpu;                     //provider specific cost of cpu execution as dollars per second
-    double Kio;                      //provider specific cost of io execution as dollars per second
+    public double Ec;                       //estimated cost of execution as dollars per program
+    public double Kcpu;                     //provider specific cost of cpu execution as dollars per second
+    public double Kio;                      //provider specific cost of io execution as dollars per second
     //Cipher
-    double Cc;                       //etimated ciphering cost as dollars per program
-    double Kc1;                      //provider specific cost of asymmetric cipher per KB
-    double Kc2;                      //provider specific cost of homomorphic cipher per KB
+    public double Cc;                       //etimated ciphering cost as dollars per program
+    public double Kc1;                      //provider specific cost of asymmetric cipher per KB
+    public double Kc2;                      //provider specific cost of homomorphic cipher per KB
 }
