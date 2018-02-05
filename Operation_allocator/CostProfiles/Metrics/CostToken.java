d6ccae80293669d43153ad1a3d8ae7f1ff0bab4f
Dario Facchinetti <dario.fad@gmail.com>
2018-02-05 11:38:13 +0100

    Complexity profiles definitions.

---
+++ Operation_allocator/CostProfiles/Metrics/CostToken.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class CostToken{
+
+    public double motionCost;
+    public double executionCost;
+    public double cipherCost;
+
+    public double totalCost;
+
+
+}
---
+++ Operation_allocator/CostProfiles/UDFprofilers/Profiler.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public abstract class Profiler{
+
+    /**
+     * A function that predicts the number of CPU operations performed by UDF or algorithm
+     * starting from a certain quantity of data
+     * @param inputSize input data
+     * @return the number of CPU operations
+     */
+    public abstract double cpuComplexityProfile(double inputSize);
+    /**
+     * A function that predicts the number of I/O operations performed by UDF or algorithm
+     * starting from a certain quantity of data
+     * @param inputSize input data
+     * @return the number of CPU operations
+     */
+    public abstract double ioComplexityProfile(double inputSize);
+
+}
